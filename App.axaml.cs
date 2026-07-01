194bc772283ec4f66e2436e0a7c5421b2cd2c39f
altesco <tetalexandr127@gmail.com>
2026-07-01 14:34:44 +0300

    added ServerService

--- App.axaml.cs
+++ App.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
+using LfoDetector.Services;
 using LfoDetector.ViewModels;
 using LfoDetector.Views;
 
@@ -23,10 +24,15 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
+
+            ServerService.StartServer("путь/к/серверу");
+
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainVM(),
             };
+
+            desktop.Exit += (_, _) => ServerService.StopServer();
         }
 
         base.OnFrameworkInitializationCompleted();
---
+++ Services/ServerService.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace LfoDetector.Services;
+
+public static class ServerService
+{
+    private static Process? _serverProcess;
+
+    public static void StartServer(string serverPath)
+    {
+        if (!File.Exists(serverPath))
+            return;
+
+        var pythonPath = "python3"; // тут как то надо поебаться с тем установлен питон в системе или нет
+
+        try
+        {
+            _serverProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = pythonPath,
+                    Arguments = serverPath,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                }
+            };
+
+            _serverProcess.OutputDataReceived += (_, e) =>
+            {
+                if (e.Data != null)
+                    Console.WriteLine($"[Python] {e.Data}");
+            };
+            _serverProcess.ErrorDataReceived += (_, e) =>
+            {
+                if (e.Data != null)
+                    Console.WriteLine($"[Python ERROR] {e.Data}");
+            };
+
+            _serverProcess.Start();
+            _serverProcess.BeginOutputReadLine();
+            _serverProcess.BeginErrorReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public static void StopServer()
+    {
+        _serverProcess?.Kill();
+    }
+}
